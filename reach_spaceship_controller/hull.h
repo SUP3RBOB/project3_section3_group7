c0b4c18fb3e5ae6b30d884be6a06d4b676741fcd
Harris <moejoe494@gmail.com>
2024-11-04 17:45:02 -0500

    Created unit tests for hull
    - Created unit tests for hull testing setters and save/load methods
    - Fixed the example tests not having the correct parameter order for QCOMPARE
    - Added comments to declarations in .h file

--- reach_spaceship_controller/hull.h
+++ reach_spaceship_controller/hull.h
@@ -3,17 +3,25 @@
 
 #include "isavable.h"
 
+// The hull module of the spaceship.
 class Hull : public ISavable {
 private:
     float integrity;
 
 public:
+    /// Creates a new hull module with max integrity (100%).
     Hull();
 
+    /// Returns the hull integrity.
     float GetIntegrity();
+
+    /// Sets the hull integrity.
     void SetIntegrity(float integrity);
 
+    /// Saves the hull integrity to a file.
     virtual void Save(QString fileName) override;
+
+    /// Loads the hull integrity from a file.
     virtual void Load(QString fileName) override;
 };
 
