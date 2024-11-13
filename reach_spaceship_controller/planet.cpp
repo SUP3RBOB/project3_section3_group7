387604f5633c66ae3f36cd108b3336c24a15ee4a
sujahat59 <157991649+sujahat59@users.noreply.github.com>
2024-11-13 14:00:44 -0500

    Update planet.cpp

--- reach_spaceship_controller/planet.cpp
+++ reach_spaceship_controller/planet.cpp
@@ -1,15 +1,15 @@
 #include "Planet.h"
 
 // Constructor to initialize name and position
-Planet::Planet(const QString &name, float x, float y) noexcept
+Planet::Planet(const QString &name, float x, float y)
     : name(name), position(x, y) {}
 
 // Getter for the planet's name
-QString Planet::GetName() const noexcept {
+QString Planet::GetName() const {
     return name;
 }
 
-// Getter for the planet's position, returns Position struct
-Planet::Position Planet::GetPosition() const noexcept {
+// Getter for the planet's position, returns QVector2D
+QVector2D Planet::GetPosition() const {
     return position;
 }
